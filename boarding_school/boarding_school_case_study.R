ddd8d926f5a0008b1747b72b25b4aa8d0d0ef8b9
jriou <julien.riou@ispm.unibe.ch>
2020-11-12 23:50:03 +0100

    Prior predictive checking

--- boarding_school/boarding_school_case_study.R
+++ boarding_school/boarding_school_case_study.R
@@ -28,7 +28,7 @@ r0 = 0
 y0 = c(s0, i0, r0)
 
 # put into list
-input_data = list(T = n_days, y0 = y0, t0 = t0, ts = t, N = N, cases = cases, switch_likelihood = 0)
+input_data = list(T = n_days, y0 = y0, t0 = t0, ts = t, N = N, cases = cases, switch_likelihood = 1)
 
 ## Sample ----
 fit = stan(file='sir_negbin.stan', 
@@ -41,6 +41,9 @@ check_hmc_diagnostics(fit)
 
 stan_trace(fit,pars=c("beta","gamma","phi"),inc_warmup=TRUE)
 ggsave(file="trace_theta.pdf",width=7,height=2.5)
+
+stan_dens(fit,pars=c("beta","gamma","phi"),separate_chains = TRUE)
+ggsave(file="post_theta.pdf",width=7,height=2.5)
 
 ## Posterior predictive check ----
 summary(fit,pars="pred_cases")[[1]] %>%
@@ -58,8 +61,38 @@ ggsave(file="inbed_fit.pdf",width=4,height=3)
 print(fit,pars=c("beta","gamma","phi","R0","recovery_time"))
 
 ## Prior predictive check ----
-inout_data$switch_likelihood == 0
+input_data$switch_likelihood = 0
 fit_prior = stan(file='sir_negbin.stan', 
                  data=input_data,
-                 chains=4,
-                 iter=1000)
+                 chains=1,
+                 iter=2000,
+                 init=0)
+
+stan_dens(fit_prior,pars=c("beta","recovery_time","phi_inv","R0","gamma","phi"),separate_chains = TRUE)
+ggsave(file="prior_theta.pdf",width=7,height=5)
+
+print(fit_prior,pars=c("beta","gamma","phi","R0","recovery_time"))
+print(fit_prior,pars=c("pred_cases"))
+summary(fit_prior,pars="pred_cases")[[1]] %>%
+  as.data.frame() %>%
+  bind_cols(cases=input_data$cases,
+            date=input_data$ts) %>%
+  ggplot() +
+  geom_ribbon(aes(x=date,ymin=`2.5%`,ymax=`97.5%`),alpha=.5,fill="grey") +
+  geom_point(aes(x=date,y=cases),shape=21,size=2.3) +
+  labs(x="Date",y="Number of students in bed") +
+  coord_cartesian(ylim=c(0,1000))
+ggsave(file="inbed_fit_prior.pdf",width=4,height=3)
+
+rstan::extract(fit_prior,pars="pred_cases")[[1]] %>%
+  as_tibble() %>%
+  rownames_to_column() %>%
+  pivot_longer(2:15) %>%
+  mutate(date=as.numeric(gsub("V","",name))) %>%
+  ggplot() +
+  geom_line(aes(x=date,y=value,group=rowname),alpha=.1) +
+  labs(x="Date",y="Number of students in bed") +
+  coord_cartesian(ylim=c(0,1000))
+ggsave(file="inbed_fit_prior_traj.pdf",width=4,height=3)
+
+    
